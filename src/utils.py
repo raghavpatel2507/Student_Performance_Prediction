aa7278c73e21c37430a13ff10453fbdb437a1a0d
raghav patel <patelraghav014@gmail.com>
2023-08-19 14:25:22 +0530

    application

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
+from src.logger import logging
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -52,4 +52,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)    
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
     
